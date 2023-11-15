034fda4a5ac3aa3150e0269bd35b517d963c85d0
swetha <93997358+swethasays@users.noreply.github.com>
2023-11-15 17:25:22 +0100

    Added comments exercise1.py

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, Float, String, Text
 
-
+#URL to fetch the data
 data_url = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
+
+#Read the CSV data into a pandas dataframe, handling any bad lines by skipping them
 df = pd.read_csv(data_url, delimiter=';', on_bad_lines='skip')
+
+#Define the data types for each column in the dataframe
 column_types = {
     'column_1': Integer(),
     'column_2': String(),
@@ -20,11 +24,18 @@ column_types = {
     'geo_punkt': String()   
 }
 
-
+#create a connection to the SQLite database
 engine = create_engine('sqlite:///airports.sqlite')
+
+#Intiliaze database metadata
 metadata = MetaData()
+
+#Define the structure of the 'airports' table
 airports = Table('airports', metadata,
                  *(Column(column_name, column_type) for column_name, column_type in column_types.items())
                  )
+#Create the table in the database
 metadata.create_all(engine)
+
+#Write the dataframe to the 'airports' table in the database, overwriting the table if it already exists
 df.to_sql('airports', con=engine, if_exists='replace', index=False)
